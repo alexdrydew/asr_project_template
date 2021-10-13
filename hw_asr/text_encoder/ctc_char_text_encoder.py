8bae253860dd78b78c73f633e7325de31fc71fbf
Сухоросов Алексей <aleksey.suhorosov@mail.ru>
2021-10-13 19:45:50 +0300

    ctc decoder

--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+from collections import defaultdict
 
 import torch
 
@@ -19,9 +20,9 @@ class CTCCharTextEncoder(CharTextEncoder):
 
     def ctc_decode(self, inds: List[int]) -> str:
         output = []
-        last_char_ind = self.char2ind[CTCCharTextEncoder.EMPTY_TOK]
+        last_char_ind = self.char2ind[self.EMPTY_TOK]
         for ind in inds:
-            if ind != self.char2ind[CTCCharTextEncoder.EMPTY_TOK] and ind != last_char_ind:
+            if ind != self.char2ind[self.EMPTY_TOK] and ind != last_char_ind:
                 output.append(ind)
             last_char_ind = ind
         return ''.join(self.ind2char[char_ind] for char_ind in output)
@@ -33,7 +34,34 @@ class CTCCharTextEncoder(CharTextEncoder):
         assert len(probs.shape) == 2
         char_length, voc_size = probs.shape
         assert voc_size == len(self.ind2char)
-        hypos = []
-        # TODO: your code here
-        raise NotImplementedError
-        return sorted(hypos, key=lambda x: x[1], reverse=True)
+
+        probs = probs.numpy()
+
+        Pb = defaultdict(lambda: 0.)
+        Pb[''] = 1.
+        Pnb = defaultdict(lambda: 0.)
+
+        beams = ['']
+        blank_ind = self.char2ind[self.EMPTY_TOK]
+
+        for t in range(char_length):
+            new_Pb = defaultdict(lambda: 0.)
+            new_Pnb = defaultdict(lambda: 0.)
+            for beam in beams:
+                for char_ind in self.ind2char:
+                    if char_ind == blank_ind:
+                        new_Pb[beam] += probs[t, blank_ind] * (Pb[beam] + Pnb[beam])
+                    elif len(beam) > 0 and self.ind2char[char_ind] == beam[-1]:
+                        new_Pnb[beam + self.ind2char[char_ind]] += probs[t, char_ind] * Pb[beam]
+                        new_Pnb[beam] += probs[t, char_ind] * Pnb[beam]
+                    else:
+                        new_Pnb[beam + self.ind2char[char_ind]] += probs[t, char_ind] * (Pb[beam] + Pnb[beam])
+            new_P = {k: new_Pb[k] + new_Pnb[k] for k in new_Pb | new_Pnb}
+            beams.clear()
+            for beam, _ in sorted(new_P.items(), key=lambda x: x[1], reverse=True)[:beam_size]:
+                beams.append(beam)
+            Pb = new_Pb
+            Pnb = new_Pnb
+
+        hyps = [(beam, Pb[beam] + Pnb[beam]) for beam in beams]
+        return sorted(hyps, key=lambda x: x[1], reverse=True)
